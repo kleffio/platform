ee0eea4740af9232b8d7242cb4ad2bee60e1c500
Chronyyx <chronyyx@gmail.com>
2026-03-13 17:40:15 -0400

    Updated what is currently in the main branch to FSD

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
-import "./index.css"
+import "@/app/styles/index.css"
 import App from "./App.tsx"
-import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { ThemeProvider } from "@/app/providers/theme-provider.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
