3d9e5030908af219dabd0402e9caba7306b9f058
Chronyyx <chronyyx@gmail.com>
2026-03-14 16:48:22 -0400

    added some values to an env file

--- dashboard/src/shared/config/oidc.ts
+++ dashboard/src/shared/config/oidc.ts
@@ -1,6 +1,6 @@
 export const oidcConfig = {
-  authority: "https://auth.kleff.io/realms/kleffio",
-  client_id: "platform-dashboard",
+  authority: import.meta.env.VITE_OIDC_AUTHORITY,
+  client_id: import.meta.env.VITE_OIDC_CLIENT_ID,
   redirect_uri: window.location.origin + "/",
   post_logout_redirect_uri: window.location.origin + "/",
   onSigninCallback: () => {
