e98c83dd3aea536f6ceeae49a927197ce9c5cc3c
Jeefos <nathan.w.roos@gmail.com>
2026-04-08 20:14:02 -0400

    removed dead code

--- internal/database/migrations/002_blueprints.sql
+++ internal/database/migrations/002_blueprints.sql
@@ -11,22 +11,32 @@ CREATE TABLE IF NOT EXISTS crates (
 );
 
 CREATE TABLE IF NOT EXISTS blueprints (
-    id               TEXT PRIMARY KEY,
-    crate_id         TEXT NOT NULL REFERENCES crates(id) ON DELETE CASCADE,
-    name             TEXT NOT NULL,
-    description      TEXT NOT NULL,
-    long_description TEXT NOT NULL DEFAULT '',
-    logo             TEXT NOT NULL DEFAULT '',
-    image            TEXT NOT NULL,
-    version          TEXT NOT NULL,
-    official         BOOLEAN NOT NULL DEFAULT false,
-    category         TEXT NOT NULL,
-    runtime_hints    JSONB NOT NULL DEFAULT '{}',
-    resources        JSONB NOT NULL DEFAULT '{}',
-    ports            JSONB NOT NULL DEFAULT '[]',
-    config           JSONB NOT NULL DEFAULT '[]',
-    outputs          JSONB NOT NULL DEFAULT '[]',
-    extensions       JSONB NOT NULL DEFAULT '{}',
-    created_at       TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated_at       TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    id           TEXT PRIMARY KEY,
+    crate_id     TEXT NOT NULL REFERENCES crates(id) ON DELETE CASCADE,
+    construct_id TEXT NOT NULL DEFAULT '',
+    name         TEXT NOT NULL,
+    description  TEXT NOT NULL,
+    logo         TEXT NOT NULL DEFAULT '',
+    version      TEXT NOT NULL,
+    official     BOOLEAN NOT NULL DEFAULT false,
+    resources    JSONB NOT NULL DEFAULT '{}',
+    config       JSONB NOT NULL DEFAULT '[]',
+    extensions   JSONB NOT NULL DEFAULT '{}',
+    created_at   TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at   TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS constructs (
+    id            TEXT PRIMARY KEY,
+    crate_id      TEXT NOT NULL REFERENCES crates(id) ON DELETE CASCADE,
+    blueprint_id  TEXT NOT NULL REFERENCES blueprints(id) ON DELETE CASCADE,
+    image         TEXT NOT NULL,
+    version       TEXT NOT NULL,
+    env           JSONB NOT NULL DEFAULT '{}',
+    ports         JSONB NOT NULL DEFAULT '[]',
+    runtime_hints JSONB NOT NULL DEFAULT '{}',
+    extensions    JSONB NOT NULL DEFAULT '{}',
+    outputs       JSONB NOT NULL DEFAULT '[]',
+    created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at    TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
